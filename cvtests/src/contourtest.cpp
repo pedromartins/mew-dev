7c0af71ff0fb79a7a2876e0895e48d2ea5b31587
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-02-02 01:44:08 +0000

    Changed project structure:
    All program code should now be written in the src folder.
    One source-file corresponds with one executable.
    Header files should go into include, if they are absolutely necessary.
    Very primitive at the moment; code in any subdirectories are ignored.
    Binaries are put into the bin/ directory.
    
    + Added ROItest - my own little program which will elaborate on =]
    + Modified the Makefile to accommodate for new directory structure.
    + Pedro's previous "test" has been renamed to contourtest
    + contourtest has been modified in some ways...

--- cvtests/src/contourtest.cpp
+++ cvtests/src/contourtest.cpp
@@ -1,10 +1,20 @@
 
-#include <stdio.h> 
+/**
+ * Mew OpenCV experiment for finding contours.
+ *
+ * @author Pedro Martins
+ */
+
+#include <stdio.h>
 
 #include <cv.h>
 #include <highgui.h>
 #include <math.h>
 
+
+/*
+ * Global variables
+ */
 IplImage * i;
 IplImage* src;
 int thresh;
@@ -13,10 +23,29 @@ CvSeq* contours;
 CvCapture * cap;
 int inited = 0;
 
+
+/**
+ * Retrieves a pixel from an IplImage. Very slow. See <INSERT NAME HERE> for
+ * a more efficient way of accessing the Image data.
+ * @param image
+ * @param x
+ * @param y
+ * @param cIdx
+ * @return
+ */
 uchar getPixel(IplImage * image, int x, int y, int cIdx) {
   return ((uchar*)(image->imageData + image->widthStep*y))[x*image->nChannels + cIdx];
 }
 
+/**
+ * Sets the values of a particular pixel in image pic.
+ * @param pic	The IplImage.
+ * @param x
+ * @param y
+ * @param r
+ * @param g
+ * @param b
+ */
 void setpixel(IplImage * pic, unsigned long x, unsigned long y, short r, short g,
 	      short b){
   // http://www710.univ-lyon1.fr/~bouakaz/OpenCV-0.9.5/docs/faq.htm
@@ -27,11 +56,16 @@ void setpixel(IplImage * pic, unsigned long x, unsigned long y, short r, short g
   temp_ptr[2] = r;
 }
 
+/**
+ * Event handler for updates to the trackbar.
+ * @param pos
+ */
 void on_trackbar(int pos)
 {
   IplImage* src1 = cvQueryFrame( cap );
   IplImage* src2 = cvQueryFrame( cap );
-  
+
+  // Create the image for blurring.
   src = cvCreateImage( cvGetSize(src1), IPL_DEPTH_8U, 3);
   cvSmooth( src1, src, CV_GAUSSIAN, 0, 0, 10, 10 );
 
@@ -40,11 +74,12 @@ void on_trackbar(int pos)
   IplImage* contoured = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 3 );
   IplImage* backup = cvCreateImage( cvGetSize(src) , IPL_DEPTH_8U, 3 );
 
-  cvThreshold(src, dst, thresh, 255, CV_THRESH_BINARY); 
+  // change the threshold.
+  cvThreshold(src, dst, thresh, 255, CV_THRESH_BINARY);
   cvSetImageCOI(dst, 1);
   cvCopy(dst, dst2, 0);
   //cvCopy(dst, backup, 0);
-  
+
   int nContours = cvFindContours( dst2, storage, &contours, sizeof(CvContour),
 				  CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0) );
   //contours = cvApproxPoly( contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, 3, 1 );
@@ -52,7 +87,7 @@ void on_trackbar(int pos)
 		  CV_RGB(0,255,0), 1, 1, 8, cvPoint(0,0));
   printf("Value: %i\n", nContours);
 
-  //cvShowImage( "Camera", src );
+  cvShowImage( "Camera", src );
   cvWaitKey(10);
   cvFlip( contoured, backup, 1);
   cvShowImage( "Image", backup );
@@ -61,14 +96,23 @@ void on_trackbar(int pos)
   cvReleaseImage( &contoured );
 }
 
+
+/**
+ * Main program
+ * Finds contours from webcam.
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main (int argc, char ** argv) {
 
   cap = cvCreateCameraCapture( CV_CAP_ANY );
   storage = cvCreateMemStorage(0);
   contours = 0;
 
+  /*
   const char* filename = argc >= 2 ? argv[1] : "test2.jpg";
-  src = cvLoadImage( filename );
+  src = cvLoadImage( filename ); */
 
   cvNamedWindow( "Image", 1 );
   cvNamedWindow( "Camera", 1 );
@@ -77,8 +121,10 @@ int main (int argc, char ** argv) {
     on_trackbar(0);
     cvWaitKey(10);
   }
-  
+
   cvWaitKey(0);
+
+  cvReleaseCapture(&cap);
 
   return 0;
 
