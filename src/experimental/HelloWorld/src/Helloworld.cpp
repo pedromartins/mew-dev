8c23019283968ab5fe5b438ee7c1b243d24141c9
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-02-10 17:04:50 +0000

    Trying to put the first few CMake files in place....

---
+++ src/experimental/HelloWorld/src/Helloworld.cpp
@@ -0,0 +1,10 @@
+
+#include "printer.hpp"
+
+using namespace std;
+
+int main(int argc, int **argv) {
+	Printer printer();
+	printer.print("Hello CMake!! You're wonderful!");
+}
+
---
+++ src/experimental/HelloWorld/src/printer.cpp
@@ -0,0 +1,9 @@
+
+#include <printer.hpp>
+#include <iostream>
+#include <string>
+
+void Printer::println(string str){
+	cout << str << endl;
+}
+
