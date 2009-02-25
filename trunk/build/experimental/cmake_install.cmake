68ea6eedcc988f59497418758b105eb6db8880cf
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-02-25 11:38:28 +0000

    More updates to the build system.
    ACTUALLY has proper CMakeLists now.

--- trunk/build/experimental/cmake_install.cmake
+++ trunk/build/experimental/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fushunpoon/dev/code/project/maple/src/simulator
+# Install script for directory: /home/fushunpoon/dev/code/project/maple/src/experimental
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,8 @@ ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  INCLUDE("/home/fushunpoon/dev/code/project/maple/build/simulator/samples/cmake_install.cmake")
+  INCLUDE("/home/fushunpoon/dev/code/project/maple/build/experimental/CVtests/cmake_install.cmake")
+  INCLUDE("/home/fushunpoon/dev/code/project/maple/build/experimental/pathfinder/cmake_install.cmake")
 
 ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
 
--- trunk/build/experimental/pathfinder/cmake_install.cmake
+++ trunk/build/experimental/pathfinder/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fushunpoon/dev/code/project/maple/src/simulator
+# Install script for directory: /home/fushunpoon/dev/code/project/maple/src/experimental/pathfinder
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -31,10 +31,4 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT)
 IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   SET(CMAKE_INSTALL_SO_NO_EXE "1")
 ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-
-IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  INCLUDE("/home/fushunpoon/dev/code/project/maple/build/simulator/samples/cmake_install.cmake")
-
-ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
 
