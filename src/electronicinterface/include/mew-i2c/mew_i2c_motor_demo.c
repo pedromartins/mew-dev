934ece697038fed6ea14bbb9182342c1048a07bb
ianpbeer <ianpbeer@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-02-25 18:18:16 +0000

    adding the first version of the working i2c headers

---
+++ src/electronicinterface/include/mew-i2c/mew_i2c_motor_demo.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include "mew_i2c.h"
+
+//example i2c program
+//shows how to use the mew_i2c library!
+//gcc -o mew_i2c_demo mew_i2c.c mew_i2c_demo.c
+
+//to initalise the library we need to know the device node
+//for the usb-i2c device
+//if this is the only device plugged in then it's probably
+// "/dev/ttyUSB0"
+//you can just grep /dev and have a look!
+
+#define DEVICE_NODE "/dev/ttyUSB1"
+
+void loop();
+
+int main(void){
+  init_comms(DEVICE_NODE);
+  loop();
+  end_comms();
+}
+
+void loop(){
+  int speed;
+  for(speed = 130; speed < 240; speed+=5){
+    write_byte_to_register(WHEEL_MOTOR_CONTROLLER, WHEEL_MOTOR_CONTROLLER_LEFT_SPEED, speed);
+    sleep(1);
+  }
+  for(speed = 240; speed > 10; speed-=5){
+    write_byte_to_register(WHEEL_MOTOR_CONTROLLER, WHEEL_MOTOR_CONTROLLER_LEFT_SPEED, speed);
+    sleep(1);
+  }
+  for(speed = 10; speed < 130; speed+=5){
+    write_byte_to_register(WHEEL_MOTOR_CONTROLLER, WHEEL_MOTOR_CONTROLLER_LEFT_SPEED, speed);
+    sleep(1);
+  }
+}
