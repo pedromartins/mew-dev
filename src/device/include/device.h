73a93715d8a5406d64aaf66361b642e5f982b434
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-04 20:25:29 +0000

    Hello.

---
+++ src/device/include/device.h
@@ -0,0 +1,15 @@
+/*
+ * device.h
+ *
+ *  Created on: 04-Mar-2009
+ *      Author: fushunpoon
+ */
+
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+#include "actuator.h"
+#include "sensor.h"
+
+
+#endif /* DEVICE_H_ */
--- src/device/include/sensor.h
+++ src/device/include/sensor.h
@@ -78,48 +78,6 @@ private:
 	DifferentialDriveSystem *drivesys;
 };
 
-/**
- * ILocator
- *
- * A locator is any means of providing a reasonable estimate of the robot's position.
- */
-class ILocator {
-	virtual ~ILocator() {}
-	virtual Vector2df getLocation() = 0;
-};
-
-/**
- * IOrientator
- *
- * An orientator is any means of providing a reasonable estimate of the robot's orientation.
- */
-class IOrientator {
-	virtual ~IOrientator() {}
-	virtual float getOrientation() = 0;
-};
-
-/**
- * IVelocimeter
- *
- * A velocimeter is any means of providing a reasonable estimate of the robot's current velocity.
- * NOTE that this is totally independent of what way the robot is facing.
- */
-class IVelocimeter {
-	virtual ~ISpedometer() {}
-	virtual Vector2df getVelocity();
-};
-
-/**
- * IAngularVelocimeter
- *
- * An angular velocimeter is any means of getting a reasonable estimate of the robot's current angular velocity.
- */
-class IAngularVelocimeter {
-	virtual ~IAngularVelocimeter() {}
-	virtual float getAngularVelocity() = 0;
-};
-
-
 
 } /// END of namespace perception
 } /// END of namespace mew
