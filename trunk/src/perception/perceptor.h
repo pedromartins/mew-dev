e58647cb1d7affc3990e1a6d32b6dc9f53ce92c6
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-04 21:50:21 +0000

    Some commits...

--- trunk/src/perception/perceptor.h
+++ trunk/src/perception/perceptor.h
@@ -1,41 +1,42 @@
-
 #ifndef _PERCEPTOR_H_
 #define _PERCEPTOR_H_
 
+#include <map>
+
 class Perceptor {
 
- public:
-  Perceptor(){
-    /*
+public:
+	Perceptor(){
+		/*
       ExampleLocator * loc = new ExampleLocator();
       locators[loc] = 0.2;
 
       ExampleOrientator * or = new ExampleOrientator();
       orientators[or] = 0.2;
-    */
-  }
+		 */
+	}
 
-  float getOrientation() {
-    float orientation = 0.0f;
-    for(std::map<ILocator *, float>::iterator iter = orientators.begin(); 
-	iter != orientators.end(); iter++) {
-      location += (*iter).first()->getOrientation() * (*iter).second();
-    }
-    return location;
-  }
+	float getOrientation() {
+		float orientation = 0.0f;
+		for(std::map<ILocator *, float>::iterator iter = orientators.begin();
+		iter != orientators.end(); iter++) {
+			location += (*iter).first()->getOrientation() * (*iter).second();
+		}
+		return location;
+	}
 
-  Vector2df getLocation() {
-    Vector2df location = new Vector2df();
-    for(std::map<ILocator *, float>::iterator iter = locators.begin();
-	iter != locators.end(); iter++) {
-      orientators += (*iter).first()->getOrientation() * (*iter).second();
-    }
-    return location;
-  }
-  
- private:
-  std::map<ILocator *, float> locators;
-  std::map<IOrientator *, float> orientators;
+	Vector2df getLocation() {
+		Vector2df location = new Vector2df();
+		for(std::map<ILocator *, float>::iterator iter = locators.begin();
+		iter != locators.end(); iter++) {
+			orientators += (*iter).first()->getOrientation() * (*iter).second();
+		}
+		return location;
+	}
+
+private:
+	std::map<ILocator *, float> locators;
+	std::map<IOrientator *, float> orientators;
 
 }
 
