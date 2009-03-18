2c7f1d6e7b9b3518f4f7f24bb903ac4553de2a83
mblokzijl <mblokzijl@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-18 21:54:42 +0000

    arduino interface MK 1, this time WITH files

---
+++ trunk/src/electronic/interface/sensors.h
@@ -0,0 +1,13 @@
+#ifndef SENSORSH
+	#define SENSORSH
+//sensor wrapper functions
+
+typedef struct
+{
+	int ir0, ir1, ir2, ir3;
+} IRreadings;
+
+IRreadings* getIRreadings(int arduino_fd);
+int getCompassreading(int arduino_fd);
+
+#endif
