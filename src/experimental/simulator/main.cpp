4de52b816dcc6f684ca825e11874b8e5c9bae02d
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-18 18:48:14 +0000

    Changes made.

---
+++ src/experimental/simulator/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 07-Mar-2009
+ *      Author: fushunpoon
+ */
+
+int main(int argc, char ** argv) {
+	Simulation sim;
+	sim.run();
+
+}
---
+++ src/experimental/simulator/simulateddecision.cpp
@@ -0,0 +1,10 @@
+/*
+ * simulateddecision.cpp
+ *
+ * A file that contains some predicates that can be
+ * expected by the decision system.
+ *
+ *  Created on: 11-Mar-2009
+ *      Author: fushunpoon
+ */
+
