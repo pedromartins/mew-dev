9c8484861c1b42cdfbd14e3e9a9f0f6bf5a2daa4
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-23 01:12:37 +0000

    rest of the random commits

--- trunk/src/experimental/simulator/samples/sample2.c
+++ trunk/src/experimental/simulator/samples/sample2.c
@@ -3,35 +3,35 @@
 #include "sim.h"
 
 void printStatus(){
-  printf("Robot at position (%d,%d)!\n", getPosition().x, getPosition().y);
-  if(isGrabbing()) printf("Grabbing!\n");
+	printf("Robot at position (%d,%d)!\n", getPosition().x, getPosition().y);
+	if(isGrabbing()) printf("Grabbing!\n");
 }
 
 int main() {
-  //loadMap(1);
-  printStatus();
-  int i;
-  // The robot will be initialized at (0,0) facing NORTH
-  for(i = 1; i != SIZE_Y ; i++) {
-    moveForward();
-    printStatus();
-  }
-  turn(FALSE); // turn right => facing EAST
-  for(i = 1; i != SIZE_X; i++) {
-    moveForward();
-    printStatus();
-  }
-  turn(FALSE); // turn right => facing SOUTH
-  for(i = 1; i != SIZE_X; i++) {
-    moveForward();
-    pickUp();
-    printStatus();
-  }
-  turn(FALSE); // turn right => facing WEST
-  for(i = 1; i != SIZE_X; i++) {
-    moveForward();
-    printStatus();
-  }
-  printf("Finished!"); 
-  return 0;
+	//loadMap(1);
+	printStatus();
+	int i;
+	// The robot will be initialized at (0,0) facing NORTH
+	for(i = 1; i != SIZE_Y ; i++) {
+		moveForward();
+		printStatus();
+	}
+	turn(FALSE); // turn right => facing EAST
+	for(i = 1; i != SIZE_X; i++) {
+		moveForward();
+		printStatus();
+	}
+	turn(FALSE); // turn right => facing SOUTH
+	for(i = 1; i != SIZE_X; i++) {
+		moveForward();
+		pickUp();
+		printStatus();
+	}
+	turn(FALSE); // turn right => facing WEST
+	for(i = 1; i != SIZE_X; i++) {
+		moveForward();
+		printStatus();
+	}
+	printf("Finished!");
+	return 0;
 }
