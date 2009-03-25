c4248db618acff768e2d019667509e4a78358b00
mblokzijl <mblokzijl@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-25 16:52:53 +0000

    Arduino interface ready for action.

--- trunk/src/electronic/interface/main.cpp
+++ trunk/src/electronic/interface/main.cpp
@@ -11,16 +11,17 @@ int main()
 
 	//get the IR headings into an array like this:
 	int ir[4];
-	//ard.getIRreadings((int*)&ir);
-	//cout << "Arduino: IR sensors: " << ir[0] << ", " << ir[1] << ", " << ir[2] << ", " << ir[3] << endl;
+	ard.getIRreadings((int*)&ir);
+	cout << "Arduino: IR sensors: " << ir[0] << ", " << ir[1] << ", " << ir[2] << ", " << ir[3] << endl;
 
 	int us[2];
-	//ard.getUSreadings((int*)&us);
-	//cout << "Arduino: Ultrasound sensors: " << us[0] << ", " << us[1] << endl;
+	ard.getUSreadings((int*)&us);
+	cout << "Arduino: Ultrasound sensors: " << us[0] << ", " << us[1] << endl;
 
 	//read the compass like this:
-	//cout << "Arduino: Compassheading: " << ard.getCompassreading() << "; " << endl;
+	cout << "Arduino: Compassheading: " << ard.getCompassreading() << "; " << endl;
 
+	
 	//Servo small test
 	cout << "Resetting Lintel" << endl;
 	ard.resetLintel();
