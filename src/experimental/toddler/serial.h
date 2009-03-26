4b823455de74c3296e3989699d95d8870a090d62
fushunpoon <fushunpoon@1fd432d8-e285-11dd-b2d9-215335d0b316>
2009-03-26 20:46:40 +0000

    committing toddler for the first time

---
+++ src/experimental/toddler/serial.h
@@ -0,0 +1,11 @@
+#ifndef SERIALH
+	#define SERIALH
+
+int serialport_writebyte( int fd, char b);
+int serialport_writeubyte( int fd, unsigned char b);
+int serialport_write(int fd, const char* str);
+int serialport_read_until(int fd, char* buf, char until);
+int serialport_init(const char* serialport, int baud);
+void serialport_close(int fd);
+
+#endif
